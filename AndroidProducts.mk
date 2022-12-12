453d27ec44a6ad807c3cd189e56ecf0ee5829362
parikk <fariqhfebrian18@gmail.com>
2022-12-12 21:15:07 +0700

    m20lte: Evolution-X

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,2 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_m20lte.mk
+    $(LOCAL_DIR)/evolution_m20lte.mk
+
+COMMON_LUNCH_CHOICES := \
+    evolution_m20lte-eng \
+    evolution_m20lte-user \
+    evolution_m20lte-userdebug
--- evolution_m20lte.mk
+++ evolution_m20lte.mk
@@ -10,9 +10,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
 # Inherit device configuration
 $(call inherit-product, device/samsung/m20lte/device.mk)
 
+# Inherit some common Evolution X stuff.
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SUPPORTS_QUICK_TAP := true
+
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := m20lte
-PRODUCT_NAME := full_m20lte
+PRODUCT_NAME := evolution_m20lte
 PRODUCT_MODEL := SM-M205F
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
