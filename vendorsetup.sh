af12b18ffba2617fb095f13c2788e3560458749e
parikk <fariqhfebrian18@gmail.com>
2023-07-29 15:11:26 +0700

    m20lte: update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -8,4 +8,4 @@ git clone https://github.com/LineageOS/android_hardware_samsung_slsi_scsc_wifibt
 git clone https://github.com/parikk/android_device_samsung_universal7904 -b 13-evox device/samsung/universal7904-common
 git clone --depth=1 https://github.com/SamarV-121/proprietary_vendor_samsung_universal7904-common -b lineage-20 vendor/samsung/universal7904-common
 git clone --depth=1 https://github.com/SamarV-121/proprietary_vendor_samsung_m20lte -b lineage-20 vendor/samsung/m20lte
-git clone --depth=1 https://github.com/SamarV-121/android_kernel_samsung_universal7904 -b lineage-20 kernel/samsung/universal7904
+git clone --depth=1 https://github.com/parikk/kernel_samsung_universal7904 -b lineage-20 kernel/samsung/universal7904
